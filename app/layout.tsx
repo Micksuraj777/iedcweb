dfd88e734521a391ef888c87519aa308038861e7
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2024-06-21 21:19:49 +0530

    add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icons/logo.jpg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

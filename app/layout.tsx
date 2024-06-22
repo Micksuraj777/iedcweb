e4521db300ce65d7ea177f2eb1c54cf54e524e1d
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2024-06-22 09:20:00 +0530

    add: logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icons/logo.jpg" />
+        <link rel="icon" href="/icons/logo.webp" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 
 import { navbarLinks } from '@/constants';
 import { cn } from '@/libs/utilis';
+import Image from 'next/image';
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -11,9 +12,12 @@ const Navbar = () => {
   return (
     <section className="flex w-full justify-between p-3 text-gray-500">
       <div className="flex flex-1 gap-6 justify-center">
-        <span className='pr-10'>
+        <span className='pr-10 flex'>
+        <Image src='/icons/logo.webp' width={50} height={50} alt='logo' />
+        <span className='flex-col'>
         <h1 className='text-gray-700 text-xl font-semibold'>IEDC CCEIJK</h1>
         <h2 className='text-gray-700 text-sm'>Beyond Boundaries</h2>
+        </span>
         </span>
         {navbarLinks.map((item) => {
           const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);

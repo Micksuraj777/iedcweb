508eac68a3179fa2263df2707d2100d8f6647d04
Mick Suraj <127860227+Micksuraj777@users.noreply.github.com>
2024-08-02 22:35:32 +0530

    modify rules and regulations

--- app/(home)/rules/page.tsx
+++ app/(home)/rules/page.tsx
@@ -3,8 +3,268 @@ import React from "react";
 
 const Rules = () => {
   return (
-    <section>
+    <section className="p-3">
+      {/* Rules section starts */}
       <PageHero pageHeroTitle="Rules & Regulations" />
+      <div className="max-w-4xl mx-auto flex flex-col md:px-8 lg:px-12">
+        <h1 className="text-black text-2xl font-semibold mb-5">RULES AND REGULATION OF IEDC</h1>
+        <h2 className="head">GENERAL</h2>
+        <p className="para">
+        General rules and regulations are applicable for all members and non-members those who are working with IEDC CCE.
+        </p>
+        <h2 className="head">IEDC</h2>
+        <p className="para">
+        IEDC CCE will be open to all existing and graduated students of Christ college of engineering.Become a member by filling the application form given at <a className="link" target="_blank" href="https://drive.google.com/file/d/1EeLxfs2EHQbPjqpkzrWrVSSNWc8qIQ4T/view">Appendix 1</a>. A fee of ₹100 will be charged for a one year membership. Application forms will be received from students upto 5th semester of B Tech only. Programmes and workshops coordinated and conducted by IEDC can avail 50-50 seats for IEDC and Non-IEDC students. IEDC members will be benefitted with special discounts for paid programs and workshops. IEDC will adopt 3 government schools to enhance their technical capabilities and to give the exposure towards Science and Technology.
+        </p>
+        <h2 className="head">
+        Innovation and Entrepreneurship Support
+        </h2>
+        <p className="para">
+        IEDC will promote and develop commercially and socially viable innovative products and solutions from the students. To accrue funding, mentorship, technical support, infra structure or any other support, students are to register for project work by filling the application form, as given at <a className="link" target="_blank" href="https://drive.google.com/file/d/1sGx1DAxZigHAbxLQQ4Uy1z351hx4AZrD/view">Appendix 2</a>. All project plans and funding plans are initially processed by the ‘IEDC Project And Prototyping Panel’. Works funded by IEDC, should handover project abstract and report to project and prototyping panel. And after each competition or exhibition they should return the prototype to the IEDC room.
+        </p>
+        <h2 className="head">
+        Funding support   
+        </h2>
+        <p className="para">
+        Funding depends on availability of funds, preference of programs and importance of projects. It will be released in 3 stages, <strong>Stage 1-</strong> immediately after approval, <strong>Stage 2-</strong> After completing 50% of work, <strong>Stage 3-</strong> After completing proposed result. If the funded work wins any cash prize in any competition, it is recommended to pay 50% of prize money to IEDC as an honour. If above mentioned 50% fall short of ‘funded amount’ a maximum of ‘prize money’ is enough to return. Afterwards submit a report containing 10 pages describing the entire details, along with an A4 size picture of the product. 
+        </p>
+        <p className="para">
+        College level project    – 30-50%
+        </p>
+        <p className="para">
+        State level project        – 30-60%
+        </p>
+        <p className="para">
+        National level project – 30-80%
+        </p>
+        <h2 className="head">
+        IEDC Room, Equipment and Tools
+        </h2>
+          <ul className="para md:pl-16 list">
+            <li>
+             To utilise the IEDC room, one should acquire prior permission from the CEO followed by the Nodal officer. 
+            </li>
+            <li>
+            IEDC room will be working only between 7:30am to 6:00pm.
+            </li>
+            <li>
+            IEDC Members’ can seek permission from any of the executive panel members with the knowledge of CEO and NO. 
+            </li>
+            <li>
+            Every entry and exit should be registered in log book. Presence inside the IEDC room without registering will be considered as an offence
+            </li>
+            <li>
+            Any equipment, working material etc… which is taken/borrowed by any student (IEDC or Non-IEDC) for any kind of work should be written in the register book and get signed from any of the panel members.
+            </li>
+            <li>
+            If the borrowed item or equipment is lost, he/she should pay the money of the item as per the market value or  can compensate with a new one.
+            </li>
+            <li>
+            Borrowed item should be returned or renewed within 15 days else fine will be imposed as per value of the product.
+            </li>
+            <li>
+            Any kind of malpractice or offences are strictly prohibited in the IEDC room. The people who do the same should face the legal action as directed by the IEDC officials or by the college authority.
+            </li>
+            <li>
+            Whoever working during the college working hours should have prior permission from the Nodal Officer and should produce it to the tutors and HODs of the respective departments.
+            </li>
+            <li>
+            Connecting electrical equipment except for laptop charger in the outlet plug present in the 3D-printer table is strictly prohibited.
+            </li>
+          </ul>
+          <h2 className="head">
+          3D Printing 
+          </h2>
+          <p className="para">
+          ENGRAVERS is an independent start-up under IEDC CCE controlled by a separate panel, headed by the CEO. It promotes ‘Earn while Learn’, and avail 25% of profit for designers and 15%of profit for printer students. 
+          </p>
+          <p className="para">
+          Running cost – ₹8/hr
+          </p>
+          <p className="para">
+          Material cost – ₹8/gram (for members) and  ₹10/gram (for non-members)
+          </p>
+          <h2 className="head">
+          FOR IEDC MEMBERS
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            The Nodal Officer followed by Assistant Nodal Officer will be the incharge and supreme authority of IEDC. The 7-member Executive panel under Nodal Officer will control and coordinate IEDC. 
+            </li>
+            <li>
+            Every member should attend monthly meetings, to be aware of the current status of IEDC. Missing it continuously twice without valid reason will affect membership. As solatium <a href="https://drive.google.com/file/d/14jGgqqT0X5IpdRcnY-yd310F4xO0wz2E/view" className="link" target="_blank">Appendix 3</a> should be filled.
+            </li>
+            <li>
+            Spending class time for any sort of activity under IEDC should be after getting signed permission <a href="https://drive.google.com/file/d/1NqVpoORGKgR2f8HRcX5HXoL52cpn5c-s/view" target="_blank" className="link">Appendix 4</a>.
+            </li>
+          </ul>
+          <h2 className="head">
+          Membership Certificate.
+          </h2>
+          <p className="para">
+          Criteria to get membership completion certificate.
+          </p>
+          <ul className="list para md:pl-16">
+            <li>
+            Should be part of any work (as volunteer, contestant or participant) in any academic year.
+            </li>
+            <li>
+            Should attend at least one program conducted by KSUM or IEDC Kerala in any academic year. 
+            </li>
+            <li>
+            Pitch an idea for an innovation or entrepreneurship, Individually or as a group of max 4 in every academic year. As to pitch register by filling the application form, as given at <a href="https://drive.google.com/file/d/1sGx1DAxZigHAbxLQQ4Uy1z351hx4AZrD/view" className="link" target="_blank">Appendix 2</a>.
+            </li>
+          </ul>
+          <h2 className="head">
+          RESPONSIBILITIES OF EACH PANELS AND TEAMS
+          </h2>
+          <h2 className="head">
+          GENERAL
+          </h2>
+          <p className="para">
+          The IEDC room key should be handled by one of the panel members or an assigned person (assigned by CEO or Nodal Officer). ExCom meetings should be conducted every odd Saturdays and members meetings every 2 months.
+          </p>
+          <h2 className="head">
+          PROJECT, INNOVATION AND PROTOTYPING PANEL
+          </h2>
+          <p className="para">
+           <strong>Head</strong>: Chief Technical Officer (<strong>CTO</strong>)
+          </p>
+          <p className="para">
+          This panel is to bridge the gap between Industry and Academia. Their duty is to identify students with potential and calibre to innovative projects and products. To support the budding Entrepreneurs through Guidelines and to create an entrepreneurship flavour in the academic fraternity. For every project a panel must consult the advisory board and CEO and should gather their permissions to proceed further.
+          </p>
+          <h2 className="head">
+          MEDIA AND PROMOTION TEAM
+          </h2>
+          <p className="para">
+          <strong>Head</strong>: Chief Publicity Officer (<strong>CPO</strong>) & Chief Marketing Officer (<strong>CMO</strong>)
+          </p>
+          <p className="para">
+          The team should consist of at least 5 members, who are good in video & photo editing software, news writing, marketing and communication. A minimum of 2 posters must be shared for every work through possible media. Always keep contact with the PRO of college. 
+          </p>
+          <h2 className="head">
+          RESPONSIBILITIES OF EXECUTIVE MEMBERS
+          </h2>
+          <h2 className="head">
+          CHIEF EXECUTIVE OFFICER (CEO)
+          </h2>
+          <p className="para">
+          CEO will be the head of ExCom and in charge of IEDC from students’ side. He/she will be the face of IEDC and bridge between Nodal Officer (NO), Kerala Start-Up Mission (KSUM), and students. 
+          </p>
+          <ul className="para list md:pl-16">
+            <li>
+            Conducting ExCom meeting every odd Saturdays. 
+            </li>
+            <li>
+            Weekly examination and reporting it to NO.  
+            </li>
+            <li>
+            Make sure every fund flow and works are recorded properly.
+            </li>
+            <li>
+            Present monthly report at ExCom meeting.
+            </li>
+            <li>
+            The communication between other IEDC officials outside the college and KSUM Officials should done by CEO.
+            </li>
+          </ul>
+          <h2 className="head">
+          CHIEF TECHINICAL OFFICER (CTO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Head of all technical activities, project works and production works.  
+            </li>
+            <li>
+            Selection and proposal of new technical events in IEDC.
+            </li>
+            <li>
+            All technical issues in IEDC are handled.
+            </li>
+            <li>
+            E-mails, permissions and connecting officials from outside for programs. 
+            </li>
+          </ul>
+          <h2 className="head">
+          CHIEF OPERATIONAL OFFICER (COO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Every event conduction or coordination by IEDC will be under the control of COO.
+            </li>
+            <li>
+            Registration for events, google form etc… related activity.
+            </li>
+            <li>
+            The yearly plan should be created with the executive committee at the beginning of the new year itself.
+            </li>
+          </ul>
+          <h2 className="head">
+          CHIEF MARKETING OFFICER (CMO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Submit a proposal to contribute ₹10,000 for IEDC in particular academic year. (Including small business, workshops, productions, investments etc…)
+            </li>
+            <li>
+            Propose new ideas to start small scale business for IEDC.
+            </li>
+            <li>
+            Should have good marketing knowledge and support upcoming entrepreneurs.   
+            </li>
+            <li>
+            Purchasing goods, momentous, certificates and other things related to IEDC shopkeeping should be managed.   
+            </li>
+          </ul>
+          <h2 className="head">
+          CHIEF CREATIVE OFFICER (CCO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Incharge for planning new events in IEDC.
+            </li>
+            <li>
+            Presenting the strategic plans and new ideas for the upcoming semester before the end of the semester or during semester break.
+            </li>
+            <li>
+            Writing and sketching related works should be done. 
+            </li>
+          </ul>
+          <h2 className="head">
+          CHIEF PUBLICITY OFFICER (CPO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Publicity and Marketing officers should go hand-in-hand.
+            </li>
+            <li>
+            Make sure each and every activity are on medias.
+            </li>
+            <li>
+            Insisting media team to make posters in IEDC banner for special days, appreciating winners, visits of special personalities at college, appreciating projects, innovative works, program participations, achievements and so on.
+            </li>
+            <li>
+            Make sure every activity is photographed and video graphed.
+            </li>
+          </ul>
+          <h2>
+          CHIEF FINANCIAL OFFICER (CFO)
+          </h2>
+          <ul className="para list md:pl-16">
+            <li>
+            Should record each and every transaction of funds in IEDC.
+            </li>
+            <li>
+            Documents like bills, chalans etc… which related to fund flow should be handled and kept safely.  
+            </li>
+            <li>
+            Incharge of stock register and guest register.
+            </li>
+            <li>
+            Yearly report for KSUM should be prepared and submitted before ExCom.
+            </li>
+          </ul>
+      </div>
     </section>
   );
 };
